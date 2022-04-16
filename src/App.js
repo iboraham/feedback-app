546a2f01e402dd1c8fd0a51724323880b4bc0daf
Onur Şerbetçi <onurserbetcii@gmail.com>
2022-04-16 22:03:30 +0100

    daily update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import Header from './components/Header'
-import Card from './components/shared/Card'
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/shared/Card";
+import Grid from "@mui/material/Grid";
+import FeedbackList from "./components/FeedbackList";
+import "./index.css";
 
 export default function App() {
-  const onMenuClick = () => {
-    console.log('Menu clicked')
-  }
+  const [reverse, setReverse] = React.useState(false);
   return (
-    <>
-    <Header title="Feedback App" onMenuClick={onMenuClick}/>
-    <Card>
-      <h1>Hello World</h1>
-    </Card>
-    </>
-  )
+    <Grid container direction="column">
+      <Grid item>
+        <Header title="Feedback App" />
+      </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <FeedbackList />
+        </Grid>
+        <Grid item xs={0} sm={2} />
+      </Grid>
+    </Grid>
+  );
 }
